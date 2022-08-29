6c2f9ef55c5061668c9074e0dd02788f2e600f4d
Qark-dev <qarkdev@gmail.com>
2022-08-29 13:08:06 +0800

    lint server side code

--- deps.ts
+++ deps.ts
@@ -1,4 +1,6 @@
-export { Router, Application, send } from "https://deno.land/x/oak@v9.0.0/mod.ts";
 export {
-  gzip,
-} from "https://deno.land/x/oak_compress@v0.0.2/mod.ts";
+  Application,
+  Router,
+  send,
+} from "https://deno.land/x/oak@v9.0.0/mod.ts";
+export { gzip } from "https://deno.land/x/oak_compress@v0.0.2/mod.ts";
--- server.ts
+++ server.ts
@@ -1,53 +1,54 @@
-import { Router, Application, send, gzip } from './deps.ts';
+import { Application, gzip, Router, send } from "./deps.ts";
 
-const index = 'index.html';
+const index = "index.html";
 const root = `${Deno.cwd()}/build`;
 const existing = await getPaths(root);
 console.log(`Existing are ${existing}`);
-	
+
 async function getPaths(currentPath: string) {
-    const names: string[] = [];
-  
-    for await (const dirEntry of Deno.readDir(currentPath)) {
-      const entryPath = `${currentPath}/${dirEntry.name}`;
+  const names: string[] = [];
 
-	  const paths: string[] = dirEntry.isDirectory
-	  	? await getPaths(entryPath)
-	  	: [entryPath.substring(root.length)];
+  for await (const dirEntry of Deno.readDir(currentPath)) {
+    const entryPath = `${currentPath}/${dirEntry.name}`;
 
-	  names.push(...paths);
-    }
-  
-    return names;
+    const paths: string[] = dirEntry.isDirectory
+      ? await getPaths(entryPath)
+      : [entryPath.substring(root.length)];
+
+    names.push(...paths);
+  }
+
+  return names;
 }
 
 const app = new Application();
 const router = new Router();
 
 router.get("/assets/images", async (ctx) => {
-	const origin = `https://graph.instagram.com/me/media?fields=id,caption,media_url\
+  const origin =
+    `https://graph.instagram.com/me/media?fields=id,caption,media_url\
 	&access_token=${Deno.env.get("ACCESS_TOKEN")}`;
 
-	await fetch(origin)
-		.then((res) => ctx.response.body = res.body)
-		.catch((err) => ctx.response.body = err);
-})
+  await fetch(origin)
+    .then((res) => ctx.response.body = res.body)
+    .catch((err) => ctx.response.body = err);
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.use(async (ctx) => {
-	let path = ctx.request.url.pathname;
-	console.log(`requested path is ${path}`)
-	if (path === "/" || !(existing.includes(path))) {
-		path = index;
-	} else {
-		console.log("it exists");
-	}
-		
-	await send(ctx, path, { root, index, });
+  let path = ctx.request.url.pathname;
+  console.log(`requested path is ${path}`);
+  if (path === "/" || !(existing.includes(path))) {
+    path = index;
+  } else {
+    console.log("it exists");
+  }
+
+  await send(ctx, path, { root, index });
 });
 
 app.use(gzip());
-	
-await app.listen({ port: 3005});
+
+await app.listen({ port: 3005 });
