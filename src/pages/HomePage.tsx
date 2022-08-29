940561ab68a861c75b884255439ce3b0cecb5500
Qark-dev <qarkdev@gmail.com>
2022-08-29 17:55:11 +0800

    use links deferred

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Project from '../components/Projects';
 import Nav from '../components/Nav';
-import pattern from '../pattern1.svg';
+import pattern from '../pattern0.svg';
 
-import arrow from '../right-arrow--yellow.png';
+const arrowBlack = 'https://imgur.com/YyT9BpB';
+const arrow = 'https://imgur.com/5PHiUIi';
 
 const HomePage: React.FC = () => {
   return (
@@ -11,9 +12,9 @@ const HomePage: React.FC = () => {
       <section className="title-sect">
         <article className="title-sect__intro">
           <h1>
-            <img className="chevron chevron--left-side" src={arrow} />
-            <code>Qatto</code>
-            <img className="chevron chevron--right-side" src={arrow} />
+            <img loading='lazy' className="chevron chevron--left-side" src={arrow} />
+            Qatto
+            <img loading='lazy' className="chevron chevron--right-side" src={arrow} />
           </h1>
           <p>Welcome to my humble abode.</p>
           <p>I&apos;m Qatto your friendly neighborhood full stacker</p>
