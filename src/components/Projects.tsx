8d72fa6c39564dd326e3e2a9f17ed838378b02da
Qark-dev <qarkdev@gmail.com>
2022-08-29 13:07:05 +0800

    theming and routing for projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,76 +1,89 @@
-import React, { useEffect } from 'react'
-import mongoose, { connect } from 'mongoose';
+import React from 'react';
 
+import triplicata from '../triplicata.png';
+import yorcloud from '../yor-cloud.png';
+import workstation from '../workstation.png';
+import maxthewaitgain from '../max-the-wait-gain.png';
 
-const uri = "mongodb+srv://mongo:P%40ssw0rd@www-qatto.ix19o.mongodb.net/?retryWrites=true&w=majority";
+import { useNavigate } from 'react-router-dom';
+import { GoGitBranch, GoLink, GoLinkExternal, GoMarkGithub } from 'react-icons/go';
 
 const Projects: React.FC = () => {
-    // useEffect(() => {
-    //     getProjects();
-    // });
+  const navigate = useNavigate();
 
-    // const getProjects = async () => {
-    //     console.log("gonna connect to db bruh")
-    //     await connect(uri);
+  const navi = (path: string) => {
+    return () => {
+      project_path(path);
+    };
+  };
 
-    //     const Schema = mongoose.Schema;
+  const project_path = (path: string) => {
+    return 'projects/' + path;
+  };
 
-    //     const BlogPost = mongoose.model('Project', new Schema({
-    //       repository: String,
-    //       project_name: String,
-    //       description: String,
-    //       art: String,
-    //     }));
+  const repos = [
+    {
+      repository: 'https://github.com/Qark-dev/Dotfiles',
+      project_name: 'workstation',
+      description: 'config for my current setup',
+      art: workstation,
+      id: 0,
+    },
+    {
+      repository: 'https://github.com/Madoshakalaka/max-the-wait-gain',
+      project_name: 'max-the-wait-gain',
+      description: "It's a fight to not eat as much",
+      art: maxthewaitgain,
+      id: 1,
+    },
+    {
+      // TODO: push the repo
+      repository: '',
+      project_name: 'triplicata',
+      description: 'Discord bot for games',
+      art: triplicata,
+      id: 2,
+    },
+    {
+      repository: 'https://github.com/Qark-dev/yorcloud_client',
+      project_name: 'yorcloud',
+      description: 'Yor favorite home cloud provider',
+      art: yorcloud,
+      id: 3,
+    },
+  ];
 
-    //     console.log(BlogPost.find({}));
-    // }
-    const getRepos = fetch('/store/projects');
+  const listItems = repos.map((r) => (
+    <article className="project" key={r.id} onClick={navi(r.project_name)}>
+      <img className="project__image" src={r.art}></img>
+      <h3 className="project__caption">{r.project_name + '   '}</h3>
+      <p className="project__caption">{r.description}</p>
+      <div className="project_references">
+        <div className="project_reference copy-link">
+          <a href="">
+            <GoLink />
+          </a>
+        </div>
+        <div className="project_reference github">
+          <a href={r.repository} className="project_github">
+            <GoGitBranch />
+          </a>
+        </div>
+        <div className="project_reference new-tab">
+          <a target="_blank" rel="noreferrer" href={project_path(r.project_name)}>
+            <GoLinkExternal />
+          </a>
+        </div>
+      </div>
+    </article>
+  ));
 
+  return (
+    <section className="projects">
+      <h2 className="projects__title">Passions and Projects </h2>
+      <ul className="projects__list">{listItems}</ul>
+    </section>
+  );
+};
 
-    const repos = [
-        { 
-            repository: "https://github.com/Qark-dev/Dotfiles",
-            project_name: "workstation",
-            description: "Dotfiles for my current workstation setup",
-            art: "https://cdn.discordapp.com/attachments/828947489267777570/997523470411903069/dot.png",
-            id: 0,
-        },
-        {
-            repository: "https://github.com/Madoshakalaka/max-the-wait-gain",
-            project_name: "max the wait gain",
-            description: "It's a fight to not eat as much",
-            art: "https://cdn.discordapp.com/attachments/828947489267777570/997525761844064296/max-the-wait-gain.png",
-            id: 1
-        },
-        {
-            repository: "",
-            project_name: "triplicata",
-            description: "Discord bot for playing games over the api",
-            art: "https://cdn.discordapp.com/attachments/828947489267777570/997522311752204360/triplicata.png",
-            id: 2
-        }
-    ];
-
-    const listItems = repos.map((r) =>
-        <article className='project' key={r.id}>
-            <a href={ r.repository }><img src={ r.art }></img></a>
-            <h3><a href={ r.repository }>{ r.project_name }
-                {/* <a href={r.repository} target='_blank' rel='noreferrer'>
-                    <img className='new-tab' src="https://cdn.discordapp.com/attachments/273539705595756544/998088503999082506/unknown.png" alt="open in new tab" />
-                </a> */}
-            </a></h3>
-            <p>{ r.description }</p>
-        </article>
-    );
-
-    return (
-        <section className='projects'>
-            <h2 className='projects__title'>Passions and Projects </h2>
-            <ul className='projects__list'>
-                { listItems }
-            </ul>
-        </section>
-    )
-}
-
-export default Projects
+export default Projects;
