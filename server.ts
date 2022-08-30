202e2a13c8321585e26bb6e69d6b5d2335bb94e0
Qark-dev <qarkdev@gmail.com>
2022-08-30 16:53:07 +0800

    add mongodb driver impl

--- server.ts
+++ server.ts
@@ -5,25 +5,65 @@ const root = `${Deno.cwd()}/build`;
 const existing = await getPaths(root);
 console.log(`Existing are ${existing}`);
 
+const mongoClient = new MongoClient();
+
 /// recursively grabs all the paths in the build directory
 async function getPaths(currentPath: string) {
   const names: string[] = [];
-
+  
   for await (const dirEntry of Deno.readDir(currentPath)) {
     const entryPath = `${currentPath}/${dirEntry.name}`;
-
+    
     const paths: string[] = dirEntry.isDirectory
-      ? await getPaths(entryPath)
-      : [entryPath.substring(root.length)];
-
+    ? await getPaths(entryPath)
+    : [entryPath.substring(root.length)];
+    
     names.push(...paths);
   }
-
+  
   return names;
 }
 
 const app = new Application();
 const router = new Router();
+
+interface ProjectSchema {
+  _id: Bson.ObjectId;
+  repository: string,
+  project_name: string,
+  description: string,
+  art: string,
+}
+
+const credential: Credential = {
+  db: 'projects',
+  username: 'mongo',
+  password: 'mongo',
+  mechanism: 'SCRAM-SHA-1',
+};
+
+const atlasConnectOptions = {
+  db: 'projects',
+  servers: [
+    {
+      host: `${Deno.env.get('MONGODB_HOST')}`,
+      port: 27017,
+    }
+  ],
+  credential,
+  tls: true,
+};
+
+await mongoClient.connect(atlasConnectOptions);
+
+router.get('/assets/projects', async (ctx) => {
+  const db = mongoClient.database("projects");
+  const projects = db.collection<ProjectSchema>("showcase");
+  
+  ctx.response.body = await projects.aggregate([
+    { $match: { repository: { $ne: null } } },
+  ]).toArray();
+});
 
 /// fetches instagram images- serving at this path
 router.get('/assets/images', async (ctx) => {
