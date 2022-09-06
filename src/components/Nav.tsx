382608d9644e5fe45d34a6841d1e62dc610a1c0d
Qark-dev <qarkdev@gmail.com>
2022-09-06 12:04:00 +0800

    new mobile navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,43 +1,61 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { GoGrabber } from 'react-icons/go';
+import useMediaQuery from "../hooks/useMediaQuery";
 
 const Nav: React.FC = () => {
-  const navboxRef = useRef<HTMLUListElement>(null);
+  const navboxRef = useRef<HTMLDivElement>(null);
+  const [isExpanded, setExpanded] = useState(false);
+  const toggleExpanded = () => {
+    setExpanded(!isExpanded);
+  }
 
   const links = ['contact', 'gallery', 'sponsor', 'hire'];
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const toggleMobile = () => {
-    if (navboxRef.current) {
-      console.log('Navbox exists');
-      navboxRef.current.toggleAttribute('nav__box--mobile');
-    } else {
-      console.error('NAVBOX IS NULL');
+  const isMobile = useMediaQuery('(max-width: 600px)');
+
+  let navClass = "nav";
+
+  const toMobile = () => {
+    navClass = navClass + " nav--mobile";
+  }
+  
+  if (isMobile) {
+    toMobile();
+
+    if (isExpanded) {
+      navClass = navClass + " nav--mobile--expanded";
     }
-  };
+  }
+
+  useEffect(() => {
+    if (!isMobile && isExpanded) {
+      setExpanded(false);
+    }
+  })
 
   return (
-    <nav className="nav nav--mobile nav--mobile--expanded">
+    <nav className={navClass}>
       <button className='home-btn'>
         <a href="/">
           <img src="favicon.ico" alt="site logo" style={{ height: '3rem', width: '3rem', imageRendering: 'pixelated' }}/>
         </a>
       </button>
-      <button className="nav-btn">
-        <GoGrabber className='nav-btn__icon' />
-      </button>
-      <ul className="nav__box" ref={navboxRef}>
-        {links.map((l, idx) => {
+      <div className="nav__box" ref={navboxRef}>
+        {(!isMobile || (isMobile && isExpanded)) && links.map((l, idx) => {
           const location = '/' + l;
-
           return (
             <button className="nav__box__item" key={idx}>
               <a href={location}>{l.toUpperCase()}</a>
             </button>
           );
         })}
-      </ul>
-      {/* <button onClick={toggleMobile}>Toggle mobile</button> */}
+      </div>
+      {
+        isMobile && 
+          <button className="nav-btn" onClick={toggleExpanded}>
+            <GoGrabber className='nav-btn__icon' />
+          </button>
+      }
     </nav>
   );
 };
