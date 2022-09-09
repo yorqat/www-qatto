1fc98d165124da0adda6fe6d4345278cf8ac5131
Qark-dev <qarkdev@gmail.com>
2022-09-09 14:09:35 +0800

    multiple mongodb hosts

--- deps.ts
+++ deps.ts
@@ -1,7 +1,11 @@
-export { Application, Router, send } from 'https://deno.land/x/oak@v9.0.0/mod.ts';
-export { gzip } from 'https://deno.land/x/oak_compress@v0.0.2/mod.ts';
 export {
-Bson,
-MongoClient
+  Application,
+  Router,
+  send,
+} from "https://deno.land/x/oak@v9.0.0/mod.ts";
+export { gzip } from "https://deno.land/x/oak_compress@v0.0.2/mod.ts";
+export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.31.0/mod.ts";
+export {
+  type Credential,
+  type ConnectOptions,
 } from "https://deno.land/x/mongo@v0.31.0/mod.ts";
-export { type Credential } from "https://deno.land/x/mongo@v0.31.0/mod.ts";
--- server.ts
+++ server.ts
@@ -1,6 +1,15 @@
-import { Application, gzip, Router, send, MongoClient, Bson, Credential } from './deps.ts';
+import {
+  Application,
+  Bson,
+  ConnectOptions,
+  Credential,
+  gzip,
+  MongoClient,
+  Router,
+  send,
+} from "./deps.ts";
 
-const index = 'index.html';
+const index = "index.html";
 const root = `${Deno.cwd()}/build`;
 const existing = await getPaths(root);
 console.log(`Existing are ${existing}`);
@@ -10,17 +19,17 @@ const mongoClient = new MongoClient();
 /// recursively grabs all the paths in the build directory
 async function getPaths(currentPath: string) {
   const names: string[] = [];
-  
+
   for await (const dirEntry of Deno.readDir(currentPath)) {
     const entryPath = `${currentPath}/${dirEntry.name}`;
-    
+
     const paths: string[] = dirEntry.isDirectory
-    ? await getPaths(entryPath)
-    : [entryPath.substring(root.length)];
-    
+      ? await getPaths(entryPath)
+      : [entryPath.substring(root.length)];
+
     names.push(...paths);
   }
-  
+
   return names;
 }
 
@@ -29,46 +38,50 @@ const router = new Router();
 
 interface ProjectSchema {
   _id: Bson.ObjectId;
-  repository: string,
-  project_name: string,
-  description: string,
-  art: string,
+  repository: string;
+  project_name: string;
+  description: string;
+  art: string;
 }
 
-const credential: Credential = {
-  db: 'projects',
-  username: 'mongo',
-  password: 'mongo',
-  mechanism: 'SCRAM-SHA-1',
-};
+console.log(
+  `mongo atlas hosts are ${Deno.env.get("MONGODB_HOSTS")!.split(":")}`,
+);
 
-const atlasConnectOptions = {
-  db: 'projects',
-  servers: [
-    {
-      host: `${Deno.env.get('MONGODB_HOST')}`,
-      port: 27017,
-    }
-  ],
-  credential,
+const atlasConnectOptions: ConnectOptions = {
+  db: "projects",
+  servers: Deno.env.get("MONGODB_HOSTS")!
+    .split(":")
+    .map(
+      (host) => {
+        return { host, port: 27017 };
+      },
+    ),
+  credential: {
+    db: "projects",
+    username: "mongo",
+    password: "mongo",
+    mechanism: "SCRAM-SHA-1",
+  },
   tls: true,
 };
 
 await mongoClient.connect(atlasConnectOptions);
 
-router.get('/assets/projects', async (ctx) => {
+router.get("/assets/projects", async (ctx) => {
   const db = mongoClient.database("projects");
   const projects = db.collection<ProjectSchema>("showcase");
-  
+
   ctx.response.body = await projects.aggregate([
     { $match: { repository: { $ne: null } } },
   ]).toArray();
 });
 
 /// fetches instagram images- serving at this path
-router.get('/assets/images', async (ctx) => {
-  const origin = `https://graph.instagram.com/me/media?fields=id,caption,media_url\
-	&access_token=${Deno.env.get('ACCESS_TOKEN')}`;
+router.get("/assets/images", async (ctx) => {
+  const origin =
+    `https://graph.instagram.com/me/media?fields=id,caption,media_url\
+	&access_token=${Deno.env.get("ACCESS_TOKEN")}`;
 
   await fetch(origin)
     .then((res) => (ctx.response.body = res.body))
@@ -81,10 +94,10 @@ app.use(router.allowedMethods());
 app.use(async (ctx) => {
   let path = ctx.request.url.pathname;
   console.log(`requested path is ${path}`);
-  if (path === '/' || !existing.includes(path)) {
+  if (path === "/" || !existing.includes(path)) {
     path = index;
   } else {
-    console.log('it exists');
+    console.log("it exists");
   }
 
   await send(ctx, path, { root, index });
