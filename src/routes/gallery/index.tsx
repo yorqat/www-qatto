ce25fde9c7ad1213d066a2e7b8b0e5b3ac409fca
Qark-dev <qarkdev@gmail.com>
2022-10-17 20:10:05 +0800

    simplify

--- src/routes/gallery/index.tsx
+++ src/routes/gallery/index.tsx
@@ -6,6 +6,7 @@ import {
 } from "@builder.io/qwik";
 import styles from "./gallery.scss";
 import TaggedList from "~/components/tagged-list/tagged-list";
+import { Link } from "@builder.io/qwik-city";
 
 interface Art {
   id: string;
@@ -15,9 +16,9 @@ interface Art {
 
 export default component$(() => {
   useStylesScoped$(styles);
-  const gallery = useStore<{ art: Art[]; active_art: Art | null }>({
+
+  const gallery = useStore<{ art: Art[] }>({
     art: [],
-    active_art: null,
   });
 
   useClientEffect$(async () => {
@@ -28,60 +29,29 @@ export default component$(() => {
 
   return (
     <>
-      {gallery.active_art && (
-        <section className="gallery-overlay">
-          <button onClick$={() => (gallery.active_art = null)}></button>
-          <img
-            className="gallery-overlay__img"
-            src={gallery.active_art.media_url}
-            alt=""
-          />
-          <h2 className="gallery-overlay__title">
-            {gallery.active_art.caption}
-          </h2>
-        </section>
-      )}
-      {!gallery.active_art && (
-        <>
-          <TaggedList
-            title="Gallery"
-            flags={["art", "assets", "design", "music"]}
-          />
-          <br></br>
-          <hr></hr>
-          <section className="gallery">
-            {gallery.art.map((dat) => {
-              return (
-                <a
-                  preventDefault:click
-                  onClick$={() => (gallery.active_art = dat)}
-                  key={dat.id}
-                >
-                  <img
-                    id={`#${dat.caption.split(" ").join("-")}`}
-                    className="gallery__item"
-                    src={dat.media_url}
-                  />
-                  {/* <p className="caption">{dat.caption}</p> */}
-                </a>
-              );
-            })}
-          </section>
-        </>
-      )}
-      {/* {gallery.active_art && (
-        <section className="gallery-overlay">
-          <h2 className="gallery-overlay__title">
-            {gallery.active_art.caption}
-            <button onClick$={() => (gallery.active_art = null)}></button>
-          </h2>
-          <img
-            className="gallery-overlay__img"
-            src={gallery.active_art.media_url}
-            alt=""
-          />
-        </section>
-      )} */}
+      <TaggedList
+        title="Gallery"
+        flags={["art", "assets", "design", "music"]}
+      />
+      <br></br>
+      <hr></hr>
+      <section className="gallery">
+        {gallery.art.map((dat) => {
+          return (
+            <Link
+              preventdefault:click
+              key={dat.id}
+              href={`/gallery/preview/${dat.id}`}
+            >
+              <img
+                id={`#${dat.caption.split(" ").join("-")}`}
+                className="gallery__item"
+                src={dat.media_url}
+              />
+            </Link>
+          );
+        })}
+      </section>
     </>
   );
 });
