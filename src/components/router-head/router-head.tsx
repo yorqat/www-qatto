2ab17930a70aa0f72b026cec417d869d144303d1
Qark-dev <qarkdev@gmail.com>
2022-11-03 19:02:10 +0800

    preconnect to instagram for now

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -15,6 +15,7 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+      <link rel="preconnect" href="https://scontent.cdninstagram.com" />
       
       {head.meta.map((m) => (
         <meta {...m} />
